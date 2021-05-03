3fcc808633968f3d995c489f3d8c7247a2fee11d
martynaut <martyna.urbanek@gmail.com>
2021-05-03 10:41:05 +0200

    remove combining vcf steps

--- run_mirnaome_analysis.py
+++ run_mirnaome_analysis.py
@@ -1,5 +1,4 @@
 import click
-from prepare_vcf_files import make_unique_files
 from extract_results_for_mirnaome import all_files_processing
 from merge_algorithms import filter_and_combine
 from distinct_occure import dist_occur
@@ -18,27 +17,21 @@ def main(input_folder,  output_folder, coordinates_file,
         from_step = 0
     from_step = int(from_step)
     if from_step <= 1:
-        click.echo("Step 1: Analysis started")
-        make_unique_files(input_folder=input_folder, output_folder=output_folder)
-        click.echo("Not unique files combined")
-    else:
-        click.echo("Skipping step 1")
-    if from_step <= 2:
-        click.echo("Step 2: Extract results for mirnaome")
+        click.echo("Step 1: Extract results for mirnaome")
         all_files_processing(input_folder, output_folder, coordinates_file)
     else:
-        click.echo("Skipping step 2")
+        click.echo("Skipping step 1")
 
-    if from_step <= 3:
-        click.echo("Step 3: Merge all algorithms")
+    if from_step <= 2:
+        click.echo("Step 2: Merge all algorithms")
         filter_and_combine(output_folder)
     else:
-        click.echo("Skipping step 3")
-    if from_step <= 4:
-        click.echo("Step 4: Make distinct and occure files")
+        click.echo("Skipping step 2")
+    if from_step <= 3:
+        click.echo("Step 3: Make distinct and occure files")
         dist_occur(output_folder, localization_file, coordinates_file)
     else:
-        click.echo("Skipping step 4")
+        click.echo("Skipping step 3")
 
     click.echo("Analysis finished")
 

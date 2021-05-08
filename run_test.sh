dcbc35c3e3bb7d87fa78571b74444df864d7d6ee
martynaut <martyna.urbanek@gmail.com>
2021-05-08 23:26:55 +0200

    run_test modification

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
-python3 run_mirnaome_analysis.py /Users/martynaurbanek/Documents/private/repos/files/mirnaome_input/LUSC \
-/Users/martynaurbanek/Documents/private/repos/files/output/DATA_LUSC \
-/Users/martynaurbanek/Documents/private/repos/files/files_for_mirnaome_mirbase22.1/coordinates.bed \
-/Users/martynaurbanek/Documents/private/repos/files/files_for_mirnaome_mirbase22.1/localizations.csv \
+python3 run_mirnaome_analysis.py /Users/username/Documents/mirnaome_input \
+/Users/username/Documents/mirnaome_output \
+/Users/username/Documents/mirnaome_run_files/coordinates.bed \
+/Users/username/Documents/mirnaome_run_files/localizations.csv \
  -s 1

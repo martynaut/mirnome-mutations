7d23210c178c711757c75260f5c78161db790388
martynaut <martyna.urbanek@gmail.com>
2021-10-08 20:17:02 +0200

    Docker files and example rename

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.8.12-bullseye
+RUN apt-get install tk-dev libpng-dev libffi-dev
+RUN useradd --create-home --shell /bin/bash app_user
+WORKDIR /home/app_user
+COPY requirements.txt /home/app_user/requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+USER app_user
+COPY . .
+CMD ["bash"]
--- run_test.sh
+++ run_test.sh
@@ -1,18 +1,18 @@
 printf "\nStarting VCF files analysis...\n\n"
 
-python3 run_mirnaome_analysis.py input_files/input_vcf/examplary_vcf_set \
-output_folder/examplary_output_from_vcf \
+python3 run_mirnaome_analysis.py input_files/input_vcf/example_vcf_set \
+output_folder/example_output_from_vcf \
 reference_files/coordinates_hg38.bed \
 reference_files/localizations_hg38.csv \
 reference_files/coordinates_with_seq_hg38.bed
 
 printf "\nStarting CSV file analysis...\n\n"
 
-python3 run_mirnaome_analysis.py input_files/input_vcf/examplary_vcf_set \
-output_folder/examplary_output_from_csv \
+python3 run_mirnaome_analysis.py input_files/input_vcf/example_vcf_set \
+output_folder/example_output_from_csv \
 reference_files/coordinates_hg38.bed \
 reference_files/localizations_hg38.csv \
 reference_files/coordinates_with_seq_hg38.bed \
--c input_files/input_csv/examplary_csv_file/examplary_csv.csv
+-c input_files/input_csv/example_csv_file/example_csv.csv
 
 printf "\nAnalyses ended.\n\n"
